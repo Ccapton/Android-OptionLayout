17f476100844de070fbc6a40a21975b2c5631137
Ccapton <437499914@qq.com>
2017-01-13 00:19:18 +0800

    Update OptionLayout.java

--- optionlayout/src/main/java/chen/capton/optionlayout/OptionLayout.java
+++ optionlayout/src/main/java/chen/capton/optionlayout/OptionLayout.java
@@ -30,16 +30,18 @@ public class OptionLayout extends HorizontalScrollView {
     private Drawable rightBgColor;
     private int rightTextSize;
 
-    private LinearLayout wrapper;
-    private ViewGroup leftLayout;
-    private LinearLayout rightLayout;
-    private Button leftOptionBtn;
-    private Button rightOptionBtn;
+    private LinearLayout wrapper;//外层布局(HorizontalScrollView的唯一子View)，包裹左边的主屏幕显示布局，右边的选项布局
+    private ViewGroup leftLayout;//左边的主屏幕显示布局
+    private LinearLayout rightLayout;//右边的选项布局
+    private Button leftOptionBtn;  //第一个选项按钮
+    private Button rightOptionBtn; //第二个选项按钮
 
     GestureDetector detector;
     GestureDetector.SimpleOnGestureListener gesturelistener;
 
-    private  int position;
+    private  int position;//保存后续设置的item位置
+    int  rigthLayoutWidth; //整个ViewGroup向左滑动的宽度
+
 
     public void setPosition(int position){
         this.position=position;
@@ -64,17 +66,15 @@ public class OptionLayout extends HorizontalScrollView {
         rightBgColor=getResources().getDrawable(R.drawable.right_btn);
         leftTextSize= (int) ta.getDimension(R.styleable.OptionLayout_leftTextSize,44);
         rightTextSize= (int) ta.getDimension(R.styleable.OptionLayout_rightTextSize,44);
-
+        ta.recycle();
 
         rightLayout=new LinearLayout(context);
-
 
         leftOptionBtn=new Button(context);
         leftOptionBtn.setBackground(leftBgColor);
         leftOptionBtn.setText(leftOptionText);
         leftOptionBtn.setTextColor(leftTextColor);
         leftOptionBtn.setTextSize(DisplayUtil.px2sp(context,leftTextSize));
-
 
         rightOptionBtn=new Button(context);
         rightOptionBtn.setBackground(rightBgColor);
@@ -87,7 +87,7 @@ public class OptionLayout extends HorizontalScrollView {
             public void onClick(View v) {
                 if(optionClicklistener!=null) {
                     optionClicklistener.leftOptionClick(v,position);
-                    smoothScrollTo(0,0);
+                    scrollTo(0,0);
                 }else {
                     Log.w("OptionLayout", "optionClicklistener is null");
                 }
@@ -98,13 +98,37 @@ public class OptionLayout extends HorizontalScrollView {
             public void onClick(View v) {
                 if(optionClicklistener!=null) {
                     optionClicklistener.rightOptionClick(v,position);
+                    scrollTo(0,0);
                 }else {
                     Log.w("OptionLayout", "optionClicklistener is null");
                 }
             }
         });
-
-        ta.recycle();
+        gesturelistener=new GestureDetector.SimpleOnGestureListener(){
+            @Override
+            public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+                if(e1.getX()-e2.getX()>=40 ){
+                    smoothScrollTo(rigthLayoutWidth,0);
+                    return false;
+                }
+                if(e2.getX()-e1.getX()>=20 ){
+                    smoothScrollTo(0,0);
+                    return false;
+                }
+                return true;
+            }
+            @Override
+            public boolean onSingleTapUp(MotionEvent e) {
+                if(optionClicklistener!=null) {
+                    optionClicklistener.itemClick(OptionLayout.this,position);
+                    scrollTo(0,0);
+                }else {
+                    Log.w("OptionLayout", "optionClicklistener is null");
+                }
+                return false;
+            }
+        };
+        detector=new GestureDetector(gesturelistener);
     }
 
     public OptionLayout(Context context, AttributeSet attrs, int defStyleAttr) {
@@ -128,10 +152,9 @@ public class OptionLayout extends HorizontalScrollView {
             lp3.height=heightSize;
             leftLayout.setLayoutParams(lp3);
 
-
-
-            Log.i("leftLayout", ": width "+widthSize);
-
+            /*
+            * 保证布局的唯一性，除了第一个子View外，其他后续的子View不可见。
+            * */
             if(wrapper.getChildCount()>1){
                for(int i=1;i<wrapper.getChildCount();i++){
                    ViewGroup childGroup= (ViewGroup) wrapper.getChildAt(i);
@@ -142,57 +165,31 @@ public class OptionLayout extends HorizontalScrollView {
                }
             }
 
-            if (rightLayout == null) {
-                Log.i("onMeasure", ": rightLayout is null");
-            } else {
-                Log.i("onMeasure", ": rightLayout is not null");
-            }
+            /*
+            * 设置选项布局的属性。宽度为屏幕宽度的0.46倍，高度与父布局相同
+            * */
             LinearLayout.LayoutParams lp2 = new LinearLayout.LayoutParams((int) (widthSize*0.46),heightSize);
             rightLayout.setLayoutParams(lp2);
+            rigthLayoutWidth=(int) (widthSize*0.46);
 
-            final int  rigthLayoutWidth=(int) (widthSize*0.46);
-
-
+             /*
+            * 设置两个选项按钮的属性。宽度等分，高度与父布局相同
+            * */
             LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(0,heightSize,1);
             leftOptionBtn.setLayoutParams(lp);
             rightOptionBtn.setLayoutParams(lp);
 
-
+            /*
+            * 添加按钮到选项布局中，添加选项布局到外层布局中
+            * */
             rightLayout.addView(leftOptionBtn);
             rightLayout.addView(rightOptionBtn);
             wrapper.addView(rightLayout);
 
-            gesturelistener=new GestureDetector.SimpleOnGestureListener(){
-                @Override
-                public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
-                    if(e1.getX()-e2.getX()>=40 ){
-                        smoothScrollTo(rigthLayoutWidth,0);
-                        return false;
-                    }
-                    if(e2.getX()-e1.getX()>=20 ){
-                        smoothScrollTo(0,0);
-                        return false;
-                    }
-                    return true;
-                }
-
-                @Override
-                public boolean onSingleTapUp(MotionEvent e) {
-                    if(optionClicklistener!=null) {
-                        optionClicklistener.itemClick(OptionLayout.this,position);
-                    }else {
-                        Log.w("OptionLayout", "optionClicklistener is null");
-                    }
-                    return false;
-                }
-            };
-            detector=new GestureDetector(gesturelistener);
             once=true;
         }
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
     }
-
-
 
     @Override
     public boolean onTouchEvent(MotionEvent ev) {
@@ -200,6 +197,9 @@ public class OptionLayout extends HorizontalScrollView {
         return true;
     }
 
+    /*
+    * 设置回调函数，让调用者实现点击事件
+    * */
     OptionClickListener optionClicklistener;
     public void setOptionClickListener(OptionClickListener listener){
         this.optionClicklistener=listener;
@@ -209,6 +209,4 @@ public class OptionLayout extends HorizontalScrollView {
         void rightOptionClick(View view, int position);
         void itemClick(View view, int position);
     }
-
-
 }
